ddbb959a272bfd7f70cfe4c45b341f8f1be94a22
tanishqk4072 <tanishqkumar4072@gmail.com>
2025-10-03 20:16:02 +0530

    Update day39question78.c

--- day39question78.c
+++ day39question78.c
@@ -1,38 +1,40 @@
-//Find the sum of main diagonal elements for a square matrix.
-
+//Find the sum of main diagonal elements for a square matrix
 #include <stdio.h>
-int main(){
+int main() {
     int x[50][50];
-    int i,j,row,col;
-    start:
-    printf("enter the amournt of rows and column : ");
-    scanf("%d %d",&row,&col);
-    if(row!=col)
-    {
-        printf(" please enter a square matrix.\n");
+    int i, j, row, col;
+
+start:
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    if (row != col) {
+        printf("Please enter a square matrix.\n");
         goto start;
     }
 
-    else
-    {
-        for(i=0;i<row;i++)
-        {
-            for(j=0;j<col;j++)
-            {
-                scanf("%d",&x[i][j]);
-            }
+    // Input matrix
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &x[i][j]);
         }
-        
-        printf("\n\n");
-        
-
     }
 
-    int sum=0;
+    int sum = 0;
 
-    for(i=0;i<row;i++)
-    {
-        sum=sum+x[i][i];
+    // Sum of both diagonals
+    for (i = 0; i < row; i++) {
+        sum += x[i][i];               // main diagonal
+        sum += x[i][row - 1 - i];     // secondary diagonal
     }
-    printf("the sum of the diagonals area : %d",sum);
+
+    // If matrix size is odd, subtract middle element once
+    if (row % 2 != 0) {
+        sum -= x[row / 2][row / 2];
     }
+
+    printf("The sum of the diagonals is: %d\n", sum);
+
+    return 0;
+}
