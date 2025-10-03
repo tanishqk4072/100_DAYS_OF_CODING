aca7ed42c323b24b0bc364147655f4dc61c9e99d
tanishqk4072 <tanishqkumar4072@gmail.com>
2025-10-03 20:20:37 +0530

    day38question76.c

--- day38question76.c
+++ day38question76.c
@@ -2,29 +2,36 @@
 
 #include <stdio.h>
 int main() {
-    int x[50][50]={0};
-    int y[50][50]={0};
-    int i,j,row,col;
-    
-    //taking rows and comlumn value
-    printf("enter the amount of rows and column: ");
-    scanf("%d %d ",&row,&col);
-    
-    //enter no  in the array
-     for (i=0;i<row;i++)
-    {
-        for (j=0;j<col;j++)
-        scanf("%d",&x[i][j]);
+    int x[50][50];
+    int i, j, row, col;
+
+    // Taking rows and column values
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+
+    if (row != col) {
+        printf("Symmetric matrix must be square!\n");
+        return 0;
     }
-    for (i=0;i<col;i++)
-    {
-        for (j=0;j<row;j++)
-            if (x[i][j]!=x[j][i])
-            {
-                printf("this is not a symmetric matrix!!");
+
+    // Input matrix
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &x[i][j]);
+        }
+    }
+
+    // Check for symmetry
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            if (x[i][j] != x[j][i]) {
+                printf("This is NOT a symmetric matrix!!\n");
                 return 0;
             }
+        }
     }
-    printf("this is a symmetric matric!!");
+
+    printf("This is a symmetric matrix!!\n");
     return 0;
 }
