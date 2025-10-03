0121e9cf353e2f156428e33dd27e48cd296314ec
tanishqk4072 <tanishqkumar4072@gmail.com>
2025-10-03 20:18:56 +0530

    day39question77.c

--- day39question77.c
+++ day39question77.c
@@ -1,42 +1,47 @@
 // Check if the elements on the diagonal of a matrix are distinct.
-
-
 #include <stdio.h>
-int main(){
+
+int main() {
     int x[50][50];
-    int i,j,row,col;
+    int i, j, row, col;
+    
     start:
-    printf("enter the amournt of rows and column : ");
-    scanf("%d %d",&row,&col);
-    if(row!=col)
-    {
-        printf(" please enter a square matrix.\n");
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &row, &col);
+    
+    if (row != col) {
+        printf("Please enter a square matrix.\n");
         goto start;
     }
 
-    else
-    {
-        for(i=0;i<row;i++)
-        {
-            for(j=0;j<col;j++)
-            {
-                scanf("%d",&x[i][j]);
-            }
+    // Input matrix
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            scanf("%d", &x[i][j]);
         }
-        
-        printf("\n\n");
     }
-    for(i=0;i<row;i++)
-    {
-        for(j=i+1;j<row;j++)
-        {
-            if(x[i][i]==x[j][j])
-            {
-                printf("the diagonals arent distinct!!");
+
+    // Check distinctness of main diagonal
+    for (i = 0; i < row; i++) {
+        for (j = i + 1; j < row; j++) {
+            if (x[i][i] == x[j][j]) {
+                printf("Main diagonal elements are not distinct!\n");
                 return 0;
             }
         }
     }
-    printf("the digonals are distinct");
-return 0;
+
+    // Check distinctness of secondary diagonal
+    for (i = 0; i < row; i++) {
+        for (j = i + 1; j < row; j++) {
+            if (x[i][row - i - 1] == x[j][row - j - 1]) {
+                printf("Secondary diagonal elements are not distinct!\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("Both diagonals have distinct elements.\n");
+    return 0;
 }
