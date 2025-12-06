404fdfe94735c32dfb4b8d51c36f219f8479b473
tanishqk4072 <tanishqkumar4072@gmail.com>
2025-12-06 18:33:20 +0530

    Add files via upload

---
+++ day40question80.c
@@ -0,0 +1,96 @@
+//#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d%d", &r1, &c1);
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d%d", &r2, &c2);
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible.\n");
+        return 0;
+    }
+    int A[50][50], B[50][50], C[50][50];
+    printf("\nEnter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("\nEnter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            C[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    printf("\nResultant Matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d%d", &r1, &c1);
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d%d", &r2, &c2);
+    if (c1 != r2) {
+        printf("Matrix multiplication not possible.\n");
+        return 0;
+    }
+
+    int A[50][50], B[50][50], C[50][50];
+    printf("\nEnter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("\nEnter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            C[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            for (int k = 0; k < c1; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    printf("\nResultant Matrix:\n");
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
